7c1d5d23a4a8c62b13883147d8743a843d8d8d09
Jinho Lee <jino2k@gmail.com>
2020-06-01 00:05:13 +0900

    updatetime.sh update

--- updatetime.sh
+++ updatetime.sh
@@ -1,34 +1,43 @@
 WORKSPACE="."
 FILE="${WORKSPACE}/updatedate.txt"
 CURRENTEPOCTIME=$((`date +"%s"`/86400))
+TODAY=`date +"%y-%m-%d"`
 
 echo "current time $CURRENTEPOCTIME"
+echo "today is $TODAY"
 
 backupsql()
 {
-  sshpass -p "sean" ssh -tt jino@192.168.0.14 '
-    echo "Hello I am a function"
-    cd /media/ssd
-    ls
-  '  
+  ls
+  # sshpass -p "sean" ssh -tt jino@192.168.0.14 '
+  #   cd /media/ssd/conf_backup/prodbackup
+  #   ./runbackup.sh
+  # '  
+}
+
+copy_backup_to_local()
+{
+  sshpass -p "sean" scp -r "jino@192.168.0.14:/media/ssd/conf_backup/prodbackup/$TODAY" "/media/jino/New Volume/conf_backup/"
 }
 
 
 if test -f "$FILE"; then
   BACKUPTIME=$(cat $FILE)
-  echo "Backup time is $BACKUPTIME"
+  echo "Last backup time is $BACKUPTIME"
   DIFF=$(($CURRENTEPOCTIME - $BACKUPTIME))
   echo "Date difference is $DIFF"
 
   if [ $DIFF -gt 6 ]; then
     backupsql
+    copy_backup_to_local
   else
     echo "Let's Skip today"
   fi
 
 else
   echo $CURRENTEPOCTIME > $FILE
-  echo "file is created"
+  echo "Timestamp file has been created"
   cat $FILE
   backupsql
+  copy_backup_to_local
 fi
